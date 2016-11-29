c077c6cafb8a48fa39a6e06333b515f48c804e84
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-29 00:10:40 -0700

    setup initial migration & seed files

---
+++ migrations/20161128164048_books.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('books', (table)=>{
+    table.increments();
+    table.string('title').defaultTo('').notNullable();
+    table.string('author').defaultTo('').notNullable();
+    table.string('genre').defaultTo('').notNullable();
+    table.text('description').defaultTo('').notNullable();
+    table.text('cover_url').defaultTo('').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('books');
+};
