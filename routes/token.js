db9af0fe801e36aa22306d6a99756179b1385728
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-12-01 17:54:30 -0700

    :construction: add basic get route for token

--- routes/token.js
+++ routes/token.js
@@ -4,11 +4,13 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const jwt=require('jsonwebtoken');
 
 // YOUR CODE HERE
 
 router.get('/token', (req, res, next) => {
-  if (req.secret) {
+  console.log(req.headers);
+  if (req.headers.tokeme) {
     res.sendStatus(200);
   } else {
     res.sendStatus(400);
